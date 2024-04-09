caf8f6047a6895dcfdc851d6d476817fe4061c24
organicessentials <159878352+organicessentials@users.noreply.github.com>
2024-04-09 13:02:43 +0530

    Update Offer.jsx

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -10,13 +10,8 @@ const Offer = () => {
     <div className="offer_sec">
       <div className="container_sec">
         <div className="offer_1">
-        <h2 className="offer_head">Free Shipping on orders above $35</h2>
-        <div className="offer_text-">
-        <span><img src={check} />Unlock 5% off with code <b>WEVALUEYOU!</b></span>
-        <span><img src={check} />Choose from convenient payment options: PayPal, Credit/Debit cards, Sezzle, and E-check.</span>
-        <span><img src={check} />Free Shipping on orders above $35</span>
-        <span><img src={check} />Shop now and embark on your journey to optimal health and vitality! <b>Offer valid till 8th April'24.</b></span>
-        </div>
+       <h2 className="offer_head">Free Shipping on orders above $35</h2>
+        <span>Embrace Nature And Start Your Organic Journey As You Take One Step Towards A Life That Is Chemical Free. We have a 5% Return Discount.</span>
        <div className="images_off">
         <a href="#products"> <img src={shop_n} /></a>
         <img src={OFFER} />
